6b00ba2bfdb9e03819a4a1753bc8119a18c50410
alessio19 <40123671+alessio19@users.noreply.github.com>
2021-07-12 19:06:07 +0200

    editMovie

--- src/Controller/EditMovieController.java
+++ src/Controller/EditMovieController.java
@@ -1,15 +1,22 @@
 package Controller;
 
+import Model.filmSession.FilmSession;
+import Model.filmSession.FilmSessionDAO;
 import Model.product.Movie;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.TextAlignment;
@@ -17,6 +24,7 @@ import javafx.scene.text.TextAlignment;
 public class EditMovieController {
     
     private Movie movie;
+    private ArrayList<FilmSession> sessions;
 
     @FXML
     private ImageView logo;
@@ -53,6 +61,12 @@ public class EditMovieController {
 
     @FXML
     private DatePicker diffusionDateEdit;
+    
+    @FXML
+    private GridPane GridSession;
+    
+    @FXML
+    private ScrollPane filmSessionPane;
 
     @FXML
     void ApplyModifMovie(MouseEvent event) {
@@ -76,7 +90,7 @@ public class EditMovieController {
 
     @FXML
     void deleteFilmSession(MouseEvent event) {
-
+        
     }
 
     public void setMovie(Movie movie) {
@@ -87,13 +101,32 @@ public class EditMovieController {
         this.titleName.setAlignment(Pos.CENTER);
         this.movieDetails.setText(this.movie.getDetails());
         this.ticketPrice.setText(Double.toString(this.movie.getTicketPrice()));
-        this.startOfDiffusion.setValue(LocalDate.parse((CharSequence) this.movie.getDiffusionStart().toString()));
-        this.endOfDiffusion.setValue(LocalDate.parse((CharSequence) this.movie.getDiffusionEnd().toString()));
+        this.startOfDiffusion.setValue(LocalDate.parse(this.movie.getDiffusionStart().toString()));
+        this.endOfDiffusion.setValue(LocalDate.parse(this.movie.getDiffusionEnd().toString()));
         this.initializeFilmSession();
     }    
     
     private void initializeFilmSession() {
-        
+        sessions = new FilmSessionDAO().getFilmSessionByMovieId(this.movie.getId());
+        for (int i=0; i<sessions.size()-1; ++i) {
+            this.GridSession.add(new Label(Integer.toString(sessions.get(i).getIdFilmSession())), 0, i+1);
+            this.GridSession.add(new Label(Integer.toString(sessions.get(i).getTicketQuantity())), 1, i+1);
+            this.GridSession.add(new Label(sessions.get(i).getDiffusionDate().toString()), 2, i+1);
+        }        
+        this.GridSession.getChildren().forEach(cell -> {
+            cell.setOnMouseClicked(e -> {
+                this.clickOnGrid(e.getSource());
+            });
+        });
+        this.GridSession.setVgap(50);
+        this.filmSessionPane.fitToWidthProperty().setValue(Boolean.TRUE);
+    }
+    
+    private void clickOnGrid(Object object) {        
+        Node n = (Node) object;
+        FilmSession session = this.sessions.get(GridPane.getRowIndex(n)-1);
+        this.quantityTicketEdit.setText(Double.toString(session.getTicketQuantity()));
+        this.diffusionDateEdit.setValue(LocalDate.parse(session.getDiffusionDate().toString()));
     }
 
 }
